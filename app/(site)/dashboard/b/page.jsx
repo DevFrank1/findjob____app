0d1e72905180018271c4d0054f89e08068126c46
DevFrank1 <fbaylarzade11@gmail.com>
2023-07-03 23:42:22 +0400

    initialization 0.0101bb

--- app/(site)/dashboard/b/page.jsx
+++ app/(site)/dashboard/b/page.jsx
@@ -1,25 +1,21 @@
-'use client'
 
-import React, { useState } from 'react';
-import { useParams } from 'next/navigation';
-import { Spinner } from '@material-tailwind/react';
-import AuthenticatedLayout from './[user]/layout';
+
+import React from 'react';
+// import { useParams } from 'next/navigation';
+// import { Spinner } from '@material-tailwind/react';
+// import AuthenticatedLayout from './[user]/layout';
 
 export default function Dashboard({ children, session }) {
 
-    const params = useParams();
-    const userParams = params.user;
-
-
-    const decodedUser = decodeURIComponent(userParams);
+ 
     // console.log(decodedUser);
 
     if (!session) {
         return <div>invalid user</div>
     }
-    if (decodedUser !== session?.user?.name) {
-        return <div>invalid user</div>
-    }
+    // if (decodedUser !== session?.user?.name) {
+    //     return <div>invalid user</div>
+    // }
     return (
         <main className='w-full h-full'>
             {children}
